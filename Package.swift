e04c29319ec29964debf81d9a97521b4a383d0d9
Logan Wright <logan.william.wright@gmail.com>
2016-02-13 11:40:23 -0500

    2nd attempt mysql

--- Package.swift
+++ Package.swift
@@ -5,6 +5,6 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/loganwright/vapor.git", majorVersion: 0),
         .Package(url: "https://github.com/loganwright/genome.git", majorVersion: 2),
-        .Package(url: "https://github.com/novi/mysql-swift.git", majorVersion: 0)
+        .Package(url: "https://github.com/loganwright/mysql-swift.git", majorVersion: 0)
     ]
 )
