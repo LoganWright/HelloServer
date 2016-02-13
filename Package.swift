6c0b2b477b0991aa66ea7abef01961b299a41879
Logan Wright <logan.william.wright@gmail.com>
2016-02-13 12:52:05 -0500

    postgres

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/loganwright/vapor.git", majorVersion: 0),
         .Package(url: "https://github.com/loganwright/genome.git", majorVersion: 2),
+        .Package(url: "https://github.com/stepanhruda/PostgreSQL-Swift.git", majorVersion: 0),
         .Package(url: "https://github.com/loganwright/mysql-swift.git", majorVersion: 0)
     ]
 )
