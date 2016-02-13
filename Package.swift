60f614dc2ab24117cf7dedaff3828de538906106
Logan Wright <logan.william.wright@gmail.com>
2016-02-13 00:53:32 -0500

    adding pure json serializer

--- Package.swift
+++ Package.swift
@@ -4,5 +4,6 @@ let package = Package(
     name: "HelloServer",
     dependencies: [
         .Package(url: "https://github.com/loganwright/vapor.git", majorVersion: 0),
+        .Package(url: "https://github.com/gfx/Swift-PureJsonSerializer.git", majorVersion: 1)
     ]
 )
--- Source/main.swift
+++ Source/main.swift
@@ -9,6 +9,89 @@
 import Foundation
 import Vapor
 
+protocol JsonSerializable {
+    
+}
+
+class JSONSerializer {
+    
+    class func serialize(object: Any) -> String {
+        
+        if let dict = object as? [String: Any] {
+            var s = "{"
+            var i = 0
+            
+            for (key, val) in dict {
+                s += "\"\(key)\":\(self.serialize(val))"
+                if i != (dict.count - 1) {
+                    s += ","
+                }
+                i += 1
+            }
+            
+            return s + "}"
+        } else if let dict = object as? [String: String] {
+            var s = "{"
+            var i = 0
+            
+            for (key, val) in dict {
+                s += "\"\(key)\":\(self.serialize(val))"
+                if i != (dict.count - 1) {
+                    s += ","
+                }
+                i += 1
+            }
+            
+            return s + "}"
+        } else if let arr = object as? [Any] {
+            var s = "["
+            
+            for i in 0 ..< arr.count {
+                s += self.serialize(arr[i])
+                
+                if i != (arr.count - 1) {
+                    s += ","
+                }
+            }
+            
+            return s + "]"
+        } else if let arr = object as? [String] {
+            var s = "["
+            
+            for i in 0 ..< arr.count {
+                s += self.serialize(arr[i])
+                
+                if i != (arr.count - 1) {
+                    s += ","
+                }
+            }
+            
+            return s + "]"
+        } else if let arr = object as? [Int] {
+            var s = "["
+            
+            for i in 0 ..< arr.count {
+                s += self.serialize(arr[i])
+                
+                if i != (arr.count - 1) {
+                    s += ","
+                }
+            }
+            
+            return s + "]"
+        } else if let string = object as? String {
+            return "\"\(string)\""
+        } else if let number = object as? Int {
+            return "\(number)"
+        } else {
+            print(object)
+            print(Mirror(reflecting: object))
+            return "\"\""
+        }
+        
+    }
+}
+
 Route.get("complex") { request in
     print("Making request: \(request)")
     let json: [String : Any] = [
@@ -19,6 +102,20 @@ Route.get("complex") { request in
     ]
     print("Sending Json: \(json)")
     return try Response(status: .OK, json: json)
+}
+
+Route.get("local-array") { _ in
+    let thing: [String : [String : Any]] = [
+        "one" : [
+            "hi" : "there"
+        ],
+        "two" : [
+            "hi" : "again",
+            "bye" : "sup"
+        ]
+    ]
+    
+    return thing
 }
 
 Route.get("hello") { request in
