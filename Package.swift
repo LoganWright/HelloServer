a3c6bb4f53397fc30b33121a77d3aaf324bc1047
Logan Wright <logan.william.wright@gmail.com>
2016-02-13 02:25:44 -0500

    trying postgres, sqlite3 doesn't work on heroku

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ let package = Package(
         .Package(url: "https://github.com/loganwright/vapor.git", majorVersion: 0),
         .Package(url: "https://github.com/loganwright/genome.git", majorVersion: 2),
         .Package(url: "https://github.com/gfx/Swift-PureJsonSerializer.git", majorVersion: 1),
-        .Package(url: "https://github.com/tannernelson/fluent.git", majorVersion: 0),
-        .Package(url: "https://github.com/loganwright/fluent-sqlite-driver.git", majorVersion: 0)
+        .Package(url: "https://github.com/Zewo/PostgreSQL.git", majorVersion: 0)
     ]
 )
