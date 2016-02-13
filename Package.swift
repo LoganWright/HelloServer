5d9450702d4034212d6b3a805f15c077e765ab85
Logan Wright <logan.william.wright@gmail.com>
2016-02-13 11:33:01 -0500

    mysql attempt

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ let package = Package(
     name: "HelloServer",
     dependencies: [
         .Package(url: "https://github.com/loganwright/vapor.git", majorVersion: 0),
-        .Package(url: "https://github.com/loganwright/genome.git", majorVersion: 2)
+        .Package(url: "https://github.com/loganwright/genome.git", majorVersion: 2),
+        .Package(url: "https://github.com/novi/mysql-swift.git", majorVersion: 0)
     ]
 )
