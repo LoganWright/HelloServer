5b411c531ad5c4dc4c5f17a8ebb8c4dd35b2d8ad
Logan Wright <logan.william.wright@gmail.com>
2016-02-13 23:17:24 -0500

    removing blackfish reference

--- Package.swift
+++ Package.swift
@@ -4,7 +4,6 @@ let package = Package(
     name: "HelloServer",
     dependencies: [
         .Package(url: "https://github.com/loganwright/vapor.git", majorVersion: 0),
-        .Package(url: "https://github.com/loganwright/genome.git", majorVersion: 2),
-        .Package(url: "https://github.com/elliottminns/blackfish.git", majorVersion: 0),
+        .Package(url: "https://github.com/loganwright/genome.git", majorVersion: 2)
     ]
 )
