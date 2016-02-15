6386d68ffb35f2d14c3769c315eedd2801f289a0
Logan Wright <logan.william.wright@gmail.com>
2016-02-14 20:41:30 -0500

    blackfish test

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ let package = Package(
     name: "HelloServer",
     dependencies: [
         .Package(url: "https://github.com/loganwright/vapor.git", majorVersion: 0),
-        .Package(url: "https://github.com/loganwright/genome.git", majorVersion: 2)
+        .Package(url: "https://github.com/loganwright/genome.git", majorVersion: 2),
+        .Package(url: "https://github.com/elliottminns/blackfish.git", majorVersion: 0)
     ]
 )
