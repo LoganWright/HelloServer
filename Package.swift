dd2ab4519827bb901dedc6a420bd60e4b8ca9217
Logan Wright <logan.william.wright@gmail.com>
2016-02-15 11:40:37 -0500

    swifter isn't well supported on heroku

--- Package.swift
+++ Package.swift
@@ -4,7 +4,6 @@ let package = Package(
     name: "HelloServer",
     dependencies: [
         .Package(url: "https://github.com/loganwright/vapor.git", majorVersion: 0),
-        .Package(url: "https://github.com/loganwright/genome.git", majorVersion: 2),
-        .Package(url: "https://github.com/loganwright/swifter.git", majorVersion: 1)
+        .Package(url: "https://github.com/loganwright/genome.git", majorVersion: 2)
     ]
 )
