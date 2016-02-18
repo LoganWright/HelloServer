ccb5a0126f6d10d700c0fb7843ade9473894687c
Logan Wright <logan.william.wright@gmail.com>
2016-02-17 21:33:13 -0500

    swifter

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ let package = Package(
     name: "HelloServer",
     dependencies: [
         .Package(url: "https://github.com/loganwright/genome.git", majorVersion: 2),
-        .Package(url: "https://github.com/loganwright/Curassow.git", majorVersion: 0, minor: 5)
+//        .Package(url: "https://github.com/loganwright/Curassow.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/loganwright/swifter.git", majorVersion: 1),
     ]
 )
--- Source/main.swift
+++ Source/main.swift
@@ -14,73 +14,85 @@ import Curassow
 import Inquiline
 import Nest
 
-let FourOhFour = Response(.Ok, contentType: "text/plain", content: "404 Not Found")
+import Swifter
 
-Routes.add(.GET, path: "/") { request in
-    let json: Json = [
-        "Hello" : "Server Side Swift",
-        "Featured Libraries" : [
-            "Genome" : "https://github.com/loganwright/genome",
-            "PureJsonSerializer" : "https://github.com/gfx/Swift-PureJsonSerializer",
-            "Curassow" : "https://github.com/kylef/curassow",
-            "Inquiline" : "https://github.com/kylef/inquiline",
-            "Nest" : "https://github.com/nestproject/nest"
-        ],
-        "BuildPack" : "https://github.com/kylef/heroku-buildpack-swift",
-        "Hosted On" : "Heroku"
-    ]
-    
-    let resp = json.serialize(.PrettyPrint)
-    return Response(.Ok, contentType: "application/json", content: resp)
-}
+let server = HttpServer()
+server["/blah"] = { _ in print("Ran"); return HttpResponse.Accepted }
+server["/home/:path"] = HttpHandlers.directoryBrowser("~/")
+server["/dumb"] = { _ in HttpResponse.OK(HttpResponseBody.Text("Hello")) }
+try server.start(8000)
 
-Routes.add(.GET, path: "/resource/:type/:name") { req in
-    print("Processing: \(req)")
-    guard
-        let name = req.arguments["name"],
-        let type = req.arguments["type"],
-        let resourceResp = ResourceResponse(status: .Ok, fileName: name, type: type)
-        else { return FourOhFour }
-    
-    print("Got resp response: \(req)")
-    return resourceResp
-}
+NSRunLoop.mainRunLoop().run()
 
-Routes.add(.GET, path: "/hello") { request in
-    return Response(.Ok, contentType: "text/plain", content: "Hello, World!\n\n\(request)")
-}
 
-// MARK: In memory for now
-
-var hello_names: [String] = []
-
-func helloHandler(request: Request) -> ResponseType {
-    guard let name = request.arguments["name"] else {
-        return FourOhFour
-    }
-    hello_names.append(name)
-    if hello_names.count > 50 {
-        hello_names = Array(hello_names.suffixFrom(1)) // Drop first
-    }
-    
-    let names: Json = Json(hello_names.map { Json($0) } )
-    let js: Json = [
-        "hello" : Json(name),
-        "meet" : names
-    ]
-    return Response(.Ok, contentType: "application/json", content: js.serialize(.PrettyPrint))
-}
-
-Routes.add(.GET, path: "/hello/:name", handler: helloHandler)
-
-// MARK: Public
-public func run(port: UInt16 = 8080) {
-    serve(port) { request in
-        return Routes.resolve(request)
-            ?? FourOhFour
-    }
-}
-
-run(9000)
+//
+//let FourOhFour = Response(.Ok, contentType: "text/plain", content: "404 Not Found")
+//
+//Routes.add(.GET, path: "/") { request in
+//    let json: Json = [
+//        "Hello" : "Server Side Swift",
+//        "Featured Libraries" : [
+//            "Genome" : "https://github.com/loganwright/genome",
+//            "PureJsonSerializer" : "https://github.com/gfx/Swift-PureJsonSerializer",
+//            "Curassow" : "https://github.com/kylef/curassow",
+//            "Inquiline" : "https://github.com/kylef/inquiline",
+//            "Nest" : "https://github.com/nestproject/nest"
+//        ],
+//        "BuildPack" : "https://github.com/kylef/heroku-buildpack-swift",
+//        "Hosted On" : "Heroku"
+//    ]
+//    
+//    let resp = json.serialize(.PrettyPrint)
+//    return Response(.Ok, contentType: "application/json", content: resp)
+//}
+//
+//Routes.add(.GET, path: "/resource/:type/:name") { req in
+//    print("Processing: \(req)")
+//    guard
+//        let name = req.arguments["name"],
+//        let type = req.arguments["type"],
+//        let resourceResp = ResourceResponse(status: .Ok, fileName: name, type: type)
+//        else { return FourOhFour }
+//    
+//    print("Got resp response: \(req)")
+//    return resourceResp
+//}
+//
+//Routes.add(.GET, path: "/hello") { request in
+//    return Response(.Ok, contentType: "text/plain", content: "Hello, World!\n\n\(request)")
+//}
+//
+//// MARK: In memory for now
+//
+//var hello_names: [String] = []
+//
+//func helloHandler(request: Request) -> ResponseType {
+//    guard let name = request.arguments["name"] else {
+//        return FourOhFour
+//    }
+//    hello_names.append(name)
+//    if hello_names.count > 50 {
+//        hello_names = Array(hello_names.suffixFrom(1)) // Drop first
+//    }
+//    
+//    let names: Json = Json(hello_names.map { Json($0) } )
+//    let js: Json = [
+//        "hello" : Json(name),
+//        "meet" : names
+//    ]
+//    return Response(.Ok, contentType: "application/json", content: js.serialize(.PrettyPrint))
+//}
+//
+//Routes.add(.GET, path: "/hello/:name", handler: helloHandler)
+//
+//// MARK: Public
+//public func run(port: UInt16 = 8080) {
+//    serve(port) { request in
+//        return Routes.resolve(request)
+//            ?? FourOhFour
+//    }
+//}
+//
+//run(8080)
 
 
