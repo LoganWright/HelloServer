73e840bde816376cbd9431bd8b235cbc9ada15b5
Logan Wright <logan.william.wright@gmail.com>
2016-02-21 09:21:26 -0500

    testing nsjson on latest toolchain

--- Source/main.swift
+++ Source/main.swift
@@ -11,6 +11,75 @@ import Vapor
 //import PureJsonSerializer
 //import Genome
 
+//check in file system
+//let filePath = Server.workDir + "Public" + request.path
+//
+//let fileManager = NSFileManager.defaultManager()
+//var isDir: ObjCBool = false
+
+//if fileManager.fileExistsAtPath(filePath, isDirectory: &isDir) {
+//    //file exists
+//    if let fileBody = NSData(contentsOfFile: filePath) {
+//        var array = [UInt8](count: fileBody.length, repeatedValue: 0)
+//        fileBody.getBytes(&array, length: fileBody.length)
+//        
+//        return Response(status: .OK, data: array, contentType: .Text)
+//    } else {
+//        handler = { _ in
+//            return Response(error: "Could not open file.")
+//        }
+//    }
+//} else {
+//    //default not found handler
+//    handler = { _ in
+//        return Response(status: .NotFound, text: "Page not found")
+//    }
+//}
+
+enum Error: ErrorType {
+    case NoFile
+}
+
+//extension Response {
+//    static func fileResponse(directory: String = "./Public", resource: String) -> Response {
+//        let filePath = directory + "/" + resource
+//        
+//        let fileManager = NSFileManager.defaultManager()
+//        var isDir: ObjCBool = false
+//        
+//        if fileManager.fileExistsAtPath(filePath, isDirectory: &isDir) {
+//            //file exists
+//            if let fileBody = NSData(contentsOfFile: filePath) {
+//                var array = [UInt8](count: fileBody.length, repeatedValue: 0)
+//                fileBody.getBytes(&array, length: fileBody.length)
+//    
+//                // Should be public
+//                return Response(status: .OK, data: array, contentType: .Text)
+//            } else {
+//                return Response(error: "Could not open file.")
+//            }
+//        } else {
+//            //default not found handler
+//            return Response(status: .NotFound, text: "Page not found")
+//        }
+//    }
+//}
+//
+//Route.get("file/:file-name") { req in
+//    guard let resourceName = req.parameters["file-name"] else {
+//        throw Error.NoFile
+//    }
+//    
+//    return Response.fileResponse(resourceName)
+//}
+
+
+extension NSData {
+    func loadResource(directory: String = "./Public", name: String) -> [UInt8] {
+        return []
+    }
+}
+
 print("SwiftServerIO -- starting")
 
 Route.get("hello") { _ in
@@ -39,6 +108,30 @@ Route.get("complex-json-test") { req in
 
 Route.post("test") { request in
     var logs: [String : String] = [:]
+    
+//    print("0")
+//    var bytes = request.data
+//    let data = NSData(bytes: &bytes, length: bytes.count)
+//    print("0.1")
+//    let ns = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
+//    print("0.1")
+//    logs["nsraw"] = "\(ns)"
+//    print("1")
+    
+    // Wouldn't allow this syntax
+    // let string = ns["test"]?["string"] as? String
+//    var value = "none"
+//    let testObject = (ns as? [String : Any])?["test"] as? [String : Any]
+//    let nsstring = testObject?["string"] as? String
+//    logs["nsstring"] = nsstring ?? "no"
+//    let nsnumber = testObject?["number"] as? Int
+//    logs["nsnumber"] = "\(nsnumber ?? -1)"
+//    let nsdouble = testObject?["double"] as? Double
+//    logs["nsdouble"] = "\(nsdouble ?? -1.0)"
+//    let nsarr = testObject?["nest"] as? [Int] ?? []
+//    logs["nsarr"] = "\(nsarr)"
+    
+    print("two")
     
     let json = try Json.deserialize(request.body)
     logs["jsraw"] = "\(json)"
