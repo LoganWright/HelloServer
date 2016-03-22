efb9f5f8bc685689b6c22d6bc62670dbd62d766a
Logan Wright <logan.william.wright@gmail.com>
2016-03-21 21:21:41 -0400

    adding simple db collecting names

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/loganwright/genome.git", majorVersion: 2),
         .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 3),
-        .Package(url: "https://github.com/loganwright/MongoKitten.git", majorVersion: 0)
+        .Package(url: "https://github.com/loganwright/MongoKitten.git", majorVersion: 0, minor: 3)
 //        .Package(url: "https://github.com/loganwright/swifter.git", majorVersion: 1),
     ]
 )
--- Source/main.swift
+++ Source/main.swift
@@ -6,13 +6,13 @@ import BSON
 // MARK: Mongo
 
 // Get real password, change database, and use heroku config variable https://devcenter.heroku.com/articles/mongolab
-let mongoUrl = "mongodb://heroku-one:testing@ds015859.mlab.com:15859"
+let mongoUrl = "mongodb://test-user:test-password@ds015859.mlab.com"
 let collection: MongoKitten.Collection?
 let errorMessage: String?
 do {
-    let server = try MongoKitten.Server(host: mongoUrl)
+    let server = try MongoKitten.Server(host: "ds015859.mlab.com", authentication: (username: "test-user", password: "test-password"), port: 15859)
     try server.connect()
-    collection = server["heroku_fdl1swgg"]["user"]
+    collection = server["heroku_fdl1swgg"]["hello-names"]
     errorMessage = ""
 } catch {
     collection = nil
@@ -54,14 +54,14 @@ public func loadResource(name: String) -> NSData? {
 let app = Application()
 //
 app.get("query", String.self) { req, name in
-    let cursor = try collection?.find("name" == name)
+    let cursor = try collection?.query("name" == name)
     let doc: Document! = nil
     if let person = cursor?.generate().next()?["name"] {
         print("Name: \(person)")
         let data = person.bsonData
         let d = NSData(bytes: data, length: data.count)
         let s = String(data: d, encoding: NSUTF8StringEncoding) ?? "OH NO"
-        return Response(status: .OK, data: person.bsonData, contentType: .Html)
+        return Response(status: .OK, data: "You found: \(s)".utf8, contentType: .Html)
     } else {
         try collection?.insert(["name" : name])
         return Response(status: .OK, text: "Machine Broke: \(errorMessage)")
@@ -78,6 +78,45 @@ app.get("ota/:product-id") { request in
 
 app.get("test") { req in
     return "Test Successful"
+}
+
+func insertIfNecessary(name: String) throws {
+    if let _ = try collection?.queryOne("name" == name)?["name"] {
+    } else {
+        try collection?.insert(["name" : name])
+    }
+}
+
+func allPeople() throws -> [String] {
+    let cursor = try collection?.query().generate()
+    var people: [String] = []
+    while let data = cursor?.next()?["name"]?.bsonData {
+        let d = NSData(bytes: data, length: data.count)
+        let s = String(data: d, encoding: NSUTF8StringEncoding) ?? "OH NO"
+        people.append(s)
+    }
+    return people
+}
+
+app.get("hello") { req in
+    return try Json(try allPeople())
+}
+
+app.get("hello", String.self) { req, name in
+    let br = "<br>"
+    var message = "Hello, \(name)!\(br)\(br)"
+    
+    let people = try allPeople()
+    var peopleMsg = "I've also said hello to:\(br)\(br)"
+    peopleMsg += people
+        .map { name -> String in return "\t\(name),\(br)" }
+        .reduce("", combine: +)
+    
+    
+    try insertIfNecessary(name)
+    
+    let html = "<p>" + message + peopleMsg + "</p>"
+    return Response.init(status: .OK, html: html)
 }
 
 app.start(port: 9090)
