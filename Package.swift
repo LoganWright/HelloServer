1c2f57a50139e1636539bac15cfe73143301383c
Logan Wright <logan.william.wright@gmail.com>
2016-03-21 22:01:04 -0400

    updating crypto for hopeful linux compiling

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "HelloServer",
     dependencies: [
         .Package(url: "https://github.com/loganwright/genome.git", majorVersion: 2),
-        .Package(url: "https://github.com/qutheory/vapor.git", versions: Version(0,3,0)...Version(0,3,0)),
+        .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 3),
         .Package(url: "https://github.com/loganwright/MongoKitten.git", majorVersion: 0, minor: 3)
 //        .Package(url: "https://github.com/loganwright/swifter.git", majorVersion: 1),
     ]
