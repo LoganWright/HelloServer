03a0894c0213c4178a899fa4b7edbd437624e2c3
Logan Wright <logan.william.wright@gmail.com>
2016-03-27 18:39:04 -0400

    rm gnm tmp

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "HelloServer",
     dependencies: [
-        .Package(url: "../Genome", majorVersion: 3),
+        // .Package(url: "../Genome", majorVersion: 3),
         .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 3),
         .Package(url: "https://github.com/loganwright/MongoKitten.git", majorVersion: 0, minor: 4)
 //        .Package(url: "https://github.com/loganwright/swifter.git", majorVersion: 1),
