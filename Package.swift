7d566cb748a8206431277dc7f3c1f8d7280b30d6
Logan Wright <logan.william.wright@gmail.com>
2016-04-01 16:26:51 -0400

    updating mongo to 0.5.x

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     dependencies: [
         // .Package(url: "../Genome", majorVersion: 3),
         .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/loganwright/MongoKitten.git", majorVersion: 0, minor: 4)
+        .Package(url: "https://github.com/planteam/MongoKitten.git", majorVersion: 0, minor: 5)
 //        .Package(url: "https://github.com/loganwright/swifter.git", majorVersion: 1),
     ]
 )
